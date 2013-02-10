d02a64c8f6b58421b0ce7077256011f3db075f73
Root Himself <root@localhost>
2013-02-10 12:41:14 +0000

    initial version

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ davclean
 ========
 
 Backup cleaner from yandex.disk
+
+This is some kind of a draft script for cleaning backups uploaded to yandex.disk. Code uses old http libraries for compatibility with old python installations on never-updated production servers.
+
+Just place .py into /usr/local/bin (don`t forget "chmod +x"!), edit .json and place in into /usr/local/etc. Feel free to make changes.
---
+++ davclean.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# coding=utf-8
+import httplib
+import urllib
+import base64
+import string
+import xml.dom.minidom
+import rfc3339_parse
+import datetime
+import sys
+import json
+
+f = open('/usr/local/etc/davclean.json', 'r')
+options = json.load(f,"utf-8")
+f.close()
+
+remotedir=urllib.quote(options["remotedir"].encode("utf-8"))
+returncode = 0
+
+conn = httplib.HTTPSConnection(options["host"])
+headers = {"Depth": "1", "Authorization": 'Basic ' + string.strip(base64.encodestring(options["user"] + ':' + options["password"]))}
+conn.request("PROPFIND", remotedir, "", headers)
+response = conn.getresponse()
+data = response.read()
+
+dom = xml.dom.minidom.parseString(data)
+responses = dom.getElementsByTagName("d:response")
+
+targeturls = []
+now = datetime.datetime.now(rfc3339_parse.UTC_TZ)
+
+for r in responses:
+	href=r.getElementsByTagName("d:href")[0].childNodes[0].nodeValue
+	cdatestr=r.getElementsByTagName("d:creationdate")[0].childNodes[0].nodeValue
+	cdatetime=rfc3339_parse.parse_datetime(cdatestr)
+	print href
+	for t in options["targets"]:
+		if href.startswith(t[0]) and ( ( now - datetime.timedelta(days=t[1])) >  cdatetime ):
+			targeturls.append(href);
+			break
+
+headers = {"Authorization": 'Basic ' + string.strip(base64.encodestring(options["user"] + ':' + options["password"]))}
+for url in targeturls:
+	try:
+		conn.request("DELETE", url, "", headers)
+		response = conn.getresponse()
+		data = response.read()
+	except Exception:
+		print type(inst)     # the exception instance
+		print inst.args      # arguments stored in .args
+		print inst           # __str__ allows args to printed directly
+		returncode = 1
+		continue
+	if response.status != httplib.OK :
+		print "Failed DELETE on " + url
+		returncode = 1
+
+sys.exit(returncode)
